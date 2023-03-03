3811c00a9cb486842c8624ba8714a0a2770a65af
unknown <kupjoseph12@gmail.com>
2023-03-03 21:26:12 +0400

    first commit

---
+++ day2/homework1.py
@@ -0,0 +1,5 @@
+name = ("Joseph")
+surname = ("Kuparadze")
+age = 25
+height = 180.25
+print(name,surname + " " +  str(age) + " " + str(height))
---
+++ day2/lesson2.py
@@ -0,0 +1,28 @@
+name = "Joseph"
+# name არის ცვლადი
+# = ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# jseph არის ცვლადისთვის მნიშვნელობა
+
+surname ="Kuparadze"
+
+
+print(name, surname)
+#print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "joseph" # ეს არის str(string) ტიპის ცვლადი
+age = 25 # ეს არის int(integer) მთელი რიცხვი
+height = 180.25 #ეს არის float ტიპის ცვლად(ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_prigraming = True #True ან False
+is_ugly = False
+
+is_cool = True
+
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+
+print(name + " " +  str(age))
